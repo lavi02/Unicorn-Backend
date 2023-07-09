8613e3ecaca9cd364c7f8e6bda5a308e64c304b7
lavi02 <18ds10535@dshs.kr>
2023-07-09 22:38:21 +0900

    [ADD:DBConnection] 각 데이터베이스 간 연결 세션 추가

--- src/database/__conn__.py
+++ src/database/__conn__.py
@@ -1,18 +1,41 @@
 import boto3
+import pymysql
+import redis
+from datetime import datetime, timedelta
 
 
-class AWSCONNECT:
+class CONNECT:
+    """
+    Returns:
+        각 연결 세션
+    """
+
     def __init__(self):
-        self.REGION: str = ""
-        self.BUCKET: str = ""
+        self.REGION: str = "ap-northeast-2"
         self.CLIENTKEY: str = ""
         self.SECRETKEY: str = ""
 
-    def s3_session(self, filePath: str, fileName: str):
+        # s3 settings
+        self.BUCKET: str = ""
+
+        # rds settings
+        self.RDSNAME: str = ""
+        self.USERNAME: str = "dev"
+        self.PASSWORD: str = ""
+        self.ENDPOINT: str = ""
+        self.DBNAME: str = "unicorn-dev"
+        self.PORT: str = "3306"
+
+        # redis settigns
+        self.REDIS_HOSTNAME: str = "localhost"
+        self.REDIS_PORT: str = "6379"
+        self.REDIS_DBNAME: str = ""
+
+    def s3Session(self, filePath: str, fileName: str):
         """
         Args:
-            filePath: 파일경로
-            fileName: 저장될 파일명
+            filePath (str): 파일경로
+            fileName (str): 저장될 파일명
         """
         try:
             s3 = boto3.client(
@@ -26,3 +49,38 @@ class AWSCONNECT:
 
         except Exception as e:
             return (str(e))
+
+    def rdsSession(self):
+        try:
+            rds = pymysql.connect(
+                host=self.ENDPOINT,
+                user=self.USERNAME,
+                password=self.PASSWORD,
+                db=self.DBNAME,
+                port=self.PORT,
+                charset="utf8"
+            )
+
+            return rds
+        except Exception as e:
+            return (str(e))
+
+    def redisConnect(self, dbname: str):
+        """
+        Args:
+            dbname (str): 사용할 db명
+        """
+        try:
+            conn = redis.StrictRedis(
+                host=self.REDIS_HOSTNAME,
+                port=self.REDIS_PORT,
+                db=self.DBNAME
+            )
+
+            return conn
+
+        except Exception as e:
+            return (str(e))
+
+
+conn = CONNECT()
