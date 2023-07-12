9570c428d678af36d4c44e72ccdf93ee479c8bee
lavi02 <zerosec7@hanyang.ac.kr>
2023-07-12 12:56:21 +0900

    [FIX:length] 스토리지 Column 길이와 해시데이터 길이가 다른 문제 수정

--- src/database/__user__.py
+++ src/database/__user__.py
@@ -7,7 +7,7 @@ class UserTable(Base): # 유저 테이블
     __tablename__ = 'user' #테이블 이름
     user_name = Column(String(50), nullable=False) # 이름
     user_id = Column(String(50), primary_key=True, nullable=False) # 아이디
-    user_pw = Column(String(50), nullable=False) # 비밀번호
+    user_pw = Column(String(100), nullable=False) # 비밀번호
     user_email = Column(String(50), nullable=False) # 이메일
     user_phone = Column(String(50), nullable=False) # 전화번호
 
